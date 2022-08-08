d5bac6e9735150879a9bd86ebee17495890db825
TAEHYEON95 <xogus950@gmail.com>
2022-08-08 22:44:21 +0900

    2022/08/08

---
+++ variable.py
@@ -0,0 +1,36 @@
+#변수 선언과 할당
+# pi = 3.14159265
+# r = 10
+
+# #변수 참조
+# print("원주율 =", pi)
+# print("반지름 =", r)
+# print("원의 둘레 =", 2 * pi * r) 
+# print("원의 넓이 =", pi * r * r)
+
+#복합 대입 연산자
+#연산자 이름         설명
+#+=          숫자 덧셈 후 대입
+# -=          숫자 뺼셈 후 대입
+# *=          숫자 곱샘 후 대입
+# /=          숫자 나눗셈 후 대입
+# %=          숫자의 나머지를 구한 후 대입
+# **=         숫자 제곱 후 대입
+
+# num = 100
+# num += 10
+# num += 20
+# num += 30
+# print("num:", num)
+
+#문자열 복합 대입 연산자
+# string = "안녕하세요"
+# string += "!"
+# string += "!"
+# print ("string :", string)
+
+input("인사말을 입력하세요>")
+
+string = input("인사")
+print(string)
+print(type(string))
