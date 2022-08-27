8bf20f3a52dfa8c29accb1e8039638e945312fb1
TAEHYEON95 <xogus950@gmail.com>
2022-08-27 18:45:20 +0900

    2022/08/27

---
+++ for_pyramid01.py
@@ -0,0 +1,7 @@
+output = ""
+
+for i in range(1,10):
+    for k in i(i):
+        output += "*"
+    output += "\n"
+print(output)
---
+++ reversed_for02.py
@@ -0,0 +1,3 @@
+#역 반복문
+for i in reversed(range(5)):
+    print (f"현재 반복 변수: {i}")
