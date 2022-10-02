7da992bada5eae0aed12114df8387867db22fb01
TAEHYEON95 <xogus950@gmail.com>
2022-10-02 17:26:13 +0900

    2022/10/02

--- taehyeon/test.py
+++ taehyeon/test.py
@@ -85,10 +85,81 @@
 #     if num == 10:
 #         break
 
-num = 0
-while num < 10:
-    num += 1 
-    if num == 5:
-        continue
-    print(num)
+# num = 0
+# while num < 10:
+#     num += 1 
+#     if num == 5:
+#         continue
+#     print(num)
+
+# user = input("사용자이름 입력>:")
+# print(user)
+
+# num1 = int(input("숫자1"))
+# num2 = int(input("숫자2"))
+# print(num1 + num2)
+
+# langs = ["한국어", "Enlgish"]
+# for i, l in enumerate(langs, start=1):
+#     print(f"{i}.{l}")
+
+# while True:
+#     sel = (input("언어를 입력하세요 : "))
+    
+#     if not sel.isnumeric():
+#         continue
+#     sel = int(sel)
+#     if 0 < sel < 3:
+#         break
+# print(f"사용자가 선택한 언어는 {langs[sel-1]}")
+
+
+# 입력한 숫자가 소수인지
+'''
+while True:
+    num = input("2 이상의 숫자를 입력 하세요> ")
+    if not num.isnumeric(): # num 이 입력 받은 값이 숫자가 아니라면 
+        continue # 돌아간다 
+    num = int(num) # num에 입력 받은 값을 문자열이 아닌 숫자형으로 캐스팅한다.
+    if num < 2: # num 의 값이 2 보다 작으면
+        continue # 돌아간다
+    break # 빠져나옴 
+
+isprime = True
+for n in range(2, num): # n에 2부터 num 입력 받은 값 까지 
+    if num % n == 0: # ex) num 값에 10을 입력했을 때 2,3,4,5,6,7,8,9 의 숫자가 각각 10을 나눠본다
+        isprime = False # 위 조건문을 판단하기 위한 변수
+        break
+
+if isprime:
+    print("소수 입니다.")
+else:
+    print("소수가 아닙니다.")
+'''
+
+while True:
+    num = input("2 이상의 숫자를 입력 하세요> ")
+    if not num.isnumeric(): # num 이 입력 받은 값이 숫자가 아니라면 
+        continue # 돌아간다 
+    num = int(num) # num에 입력 받은 값을 문자열이 아닌 숫자형으로 캐스팅한다.
+    if num < 2: # num 의 값이 2 보다 작으면
+        continue # 돌아간다
+    break # 빠져나옴 
+
+prime_list = [False,False] + [True] * (num -1) 
+# num이 5라면 F:F:T:T:T:T
+prims = []
+
+for i in range(2, num + 1): #num이 5라면 2 ~ 6
+    if prime_list[i]: #F:F:T:T:T:T
+        for j in range(2 * i, num + 1, i):
+            prime_list[j] = False
+
+prims = [i for i in range(2, num+1) if prime_list[i] == True]
+print(prims)
+
+if num in prims:
+    print("소수 입니다.")
+else:
+    print("소수가 아닙니다.")
 
---
+++ taehyeon/test2.py
@@ -0,0 +1,13 @@
+# 파이썬에서 파일 읽고 쓰기
+
+
+
+file = open("sample.txt", mode="w", encoding="utf-8")
+file.write("안녕파이썬")
+file.close()
+
+# rfile = open("sample.txt", mode="r")
+# content = rfile.read()
+# rfile.close()
+
+# print(content)
